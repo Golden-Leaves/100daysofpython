8080371a0a2db8323d4bed4377c3b61933b49ede
Golden_Leaves <letuongminh10@gmail.com>
2025-03-21 13:10:50 +0700

    add Procfile to blog_v5

--- flask_framework/blog_v5/main.py
+++ flask_framework/blog_v5/main.py
@@ -10,6 +10,8 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+from dotenv import load_dotenv
+import os
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
@@ -27,9 +29,13 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+env_path = current_directory + r"\.env"
+load_dotenv(env_path)
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -56,7 +62,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URI","sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
