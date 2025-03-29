c071f3b8d70b95c1949e6fd5af6f9cf0a73aaad6
Golden_Leaves <letuongminh10@gmail.com>
2025-03-29 19:22:50 +0700

    Modify loading logic

--- college_major_salaries/payscale_data.py
+++ college_major_salaries/payscale_data.py
@@ -7,6 +7,7 @@ from selenium.webdriver import ActionChains
 import time
 import csv
 def main():
+    MAGIC_SCROLL_MULTIPLIER = 0.1
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option("detach",True)
     driver = webdriver.Chrome(options=chrome_options)
@@ -16,18 +17,20 @@ def main():
     data = []
     #Scroll so that the table is visible
     html = driver.find_element(By.TAG_NAME, "html")
-    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight * 0.1", html)
+    # driver.execute_script(f"arguments[0].scrollTop = arguments[0].scrollHeight * {MAGIC_SCROLL_MULTIPLIER}", html)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 
-    table_rows = driver.find_elements(By.TAG_NAME,value="tr")
+    table_rows = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME,"tr")))
     for row in table_rows:
         row_data = []
-        major_cell = row.find_element(By.CSS_SELECTOR, ".data-table__cell.csr-col--school-name")
-        others  = row.find_elements(By.CSS_SELECTOR,value=".data-table__cell.csr-col--right ")
-        non_rank_cells= [major_cell] + others #others is a list lol
-        for non_rank_cell in non_rank_cells:
-            cell_value = non_rank_cell.find_element(By.CSS_SELECTOR,value=".data-table__value")
-            row_data.append(cell_value)
-        data.append(row_data)
+        major_cell = row.find_element(By.CSS_SELECTOR, ".data-table__cell.csr-col--school-name .data-table__value")
+        others  = row.find_elements(By.CSS_SELECTOR,value=".data-table__cell.csr-col--right .data-table__value")[0:2] #The other columns
+        non_rank_cells= [major.getText() for major in major_cell] + [other.getText() for other in others] #others is a list lol
+        print(non_rank_cells)
+        # for non_rank_cell in non_rank_cells:
+        #     cell_value = non_rank_cell.find_element(By.CSS_SELECTOR,value=".data-table__value")
+        #     row_data.append(cell_value)
+        # data.append(row_data)
     
   
     
