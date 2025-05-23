71e91e4b7c12ea9b2c8ba2c75cfe7f7c0b230909
Golden_Leaves <letuongminh10@gmail.com>
2025-05-23 22:03:51 +0700

    nobel_prize_a complete Prizes by Country over Time

---
+++ data_science/nobel_prize_analysis/test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from datetime import datetime
+
+# Time series data
+dates = pd.date_range(start='20200101', periods=4)
+values = [10, 20, -10, 5]
+
+df = pd.DataFrame({'Values': values}, index=dates)
+cumulative_sum = df['Values'].cumsum()
+print(df)
