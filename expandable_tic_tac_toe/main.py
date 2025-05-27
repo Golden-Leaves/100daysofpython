51e6157b31ee920bd556c780105702a76dbf2b3e
Golden_Leaves <letuongminh10@gmail.com>
2025-05-27 20:46:47 +0700

    Add expandable_tic_tac_toe

---
+++ expandable_tic_tac_toe/main.py
@@ -0,0 +1,12 @@
+def main():
+    dimension = "3x3".split("x")
+    rows = int(dimension[0])
+    columns = int(dimension[1])
+    board = [" " * (rows*columns)]
+    # for i in range(0,rows + 1):
+    #     if 
+    #     print(f"  |" * columns)
+    print("board")
+
+if __name__ == "__main__":
+    main()
