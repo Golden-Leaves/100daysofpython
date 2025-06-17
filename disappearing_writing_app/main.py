110ae1a60d91832fdb92542f0280e8a49de7da0f
Golden_Leaves <letuongminh10@gmail.com>
2025-06-17 17:47:31 +0700

    Load bootstrap template

---
+++ disappearing_writing_app/main.py
@@ -0,0 +1,60 @@
+from datetime import date
+from flask import Flask, abort, render_template, redirect, url_for, flash
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text,ForeignKey
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+# from forms import CreatePostForm,RegisterForm,LoginForm,CommentForm
+import os
+from dotenv import load_dotenv
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+env_path = os.path.join(current_directory,".env")
+print(env_path)
+load_dotenv(env_path)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+ckeditor = CKEditor(app)
+
+Bootstrap5(app)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return db.session.execute(db.select(User).where(User.id == user_id)).scalar()
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+#Decorator to check for admin status
+def admin_only(func):
+    @wraps(func)
+    def wrapper(*args,**kwargs):
+        if current_user.id != 1:
+            abort(403)
+        else:
+            return func(*args,**kwargs)
+    return wrapper
+
+
+class User(db.Model,UserMixin):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(250),unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(250), nullable=False)
